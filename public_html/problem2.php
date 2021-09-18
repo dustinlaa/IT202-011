b42cd5e307e6e81233d98253f0cbbd4ed3b57066
dustinlaa <drl3@njit.edu>
2021-09-17 21:31:45 -0400

    added code in problem 3 to output each number as positive

--- public_html/problem2.php
+++ public_html/problem2.php
@@ -13,7 +13,7 @@ function getTotal($arr) {
     }
     //TODO do rounding stuff here
     $total = round($total, 2);
-
+    
     echo "The total is " . var_export($total, true);
 }
 echo "Problem 2: Adding Floats<br>";
