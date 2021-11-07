87aa25f8888d4495dc0ba5daa904346d6e434e90
Dustin La <71194959+dustinlaa@users.noreply.github.com>
2021-11-07 16:01:00 -0500

    Revert "milestone1 (bootstrap, styling, and other fixes)"

--- public_html/Project/logout.php
+++ public_html/Project/logout.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
 require(__DIR__ . "/../../lib/functions.php");
-require_once(__DIR__ . "/../../partials/nav.php");
+reset_session();
 
 flash("Successfully logged out", "success");
-
-die(header("Location: login.php"));
+header("Location: login.php");
