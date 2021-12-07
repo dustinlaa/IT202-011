2c825194ac72011c76a1ae20e49112d932ebb81b
dustinlaa <drl3@njit.edu>
2021-12-06 19:02:50 -0500

    redoing competitionparticipants

--- public_html/Project/sql/010_create_table_competitionparticipants.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS CompetitionParticipants(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    comp_id int,
-    user_id int,
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY (comp_id, user_id), -- a user may only join a particular competition once
-    FOREIGN KEY (comp_id) REFERENCES Competitions(id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
-)
