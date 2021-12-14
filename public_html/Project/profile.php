5d372312fc7c8acd72debc1a042a2f4fadedd672
dustinlaa <drl3@njit.edu>
2021-12-14 11:49:53 -0500

    users can now set their profile to public or private

--- public_html/Project/profile.php
+++ public_html/Project/profile.php
@@ -124,9 +124,9 @@ try {
     <h1>Profile</h1>
     <?php if ($isMe) : ?>
         <?php if ($edit) : ?>
-            <a class="btn btn-primary" href="?">View</a>
+            <a class="btn btn-dark" href="?">View</a>
         <?php else : ?>
-            <a class="btn btn-primary" href="?edit=true">Edit</a>
+            <a class="btn btn-dark" href="?edit=true">Edit</a>
         <?php endif; ?>
     <?php endif; ?>
     <div>
@@ -150,31 +150,45 @@ try {
             </tbody>
         </table>
     </div>
-    <form method="POST" onsubmit="return validate(this);">
-        <div class="mb-3">
-            <label class="form-label" for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" value="<?php se($email); ?>" />
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="username">Username</label>
-            <input class="form-control" type="text" name="username" id="username" value="<?php se($username); ?>" />
-        </div>
-        <!-- DO NOT PRELOAD PASSWORD -->
-        <div class="mb-3">Password Reset</div>
-        <div class="mb-3">
-            <label class="form-label" for="cp">Current Password</label>
-            <input class="form-control" type="password" name="currentPassword" id="cp" />
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="np">New Password</label>
-            <input class="form-control" type="password" name="newPassword" id="np" />
-        </div>
-        <div class="mb-3">
-            <label class="form-label" for="conp">Confirm Password</label>
-            <input class="form-control" type="password" name="confirmPassword" id="conp" />
-        </div>
-        <input type="submit" class="mt-3 btn btn-dark" value="Update Profile" name="save" />
-    </form>
+    <?php if (!$edit) : ?>
+        <div>Username: <?php se($username); ?></div>
+        <div>Joined: <?php se($created); ?></div>
+        <!-- TODO any other public info -->
+    <?php endif; ?>
+
+    <?php if ($isMe && $edit) : ?>
+        <form method="POST" onsubmit="return validate(this);">
+            <div class="mb-3">
+                <div class="form-check form-switch">
+                    <input name="visibility" class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" <?php if ($public) echo "checked"; ?>>
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Make Profile Public</label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-control" type="email" name="email" id="email" value="<?php se($email); ?>" />
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="username">Username</label>
+                <input class="form-control" type="text" name="username" id="username" value="<?php se($username); ?>" />
+            </div>
+            <!-- DO NOT PRELOAD PASSWORD -->
+            <div class="mb-3">Password Reset</div>
+            <div class="mb-3">
+                <label class="form-label" for="cp">Current Password</label>
+                <input class="form-control" type="password" name="currentPassword" id="cp" />
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="np">New Password</label>
+                <input class="form-control" type="password" name="newPassword" id="np" />
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="conp">Confirm Password</label>
+                <input class="form-control" type="password" name="confirmPassword" id="conp" />
+            </div>
+            <input type="submit" class="mt-3 btn btn-dark" value="Update Profile" name="save" />
+        </form>
+    <?php endif; ?>
 </div>
 <script>
     function validate(form) {
